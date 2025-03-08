0cfcdc41fc84db22f1deb8d0351096db22b12656
raman-dxxp <ramandeep1568@conestogac.on.ca>
2025-03-07 22:31:25 -0500

    Add project files.

---
+++ HealthBridge_TechCursaders/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HealthBridge_TechCursaders.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
